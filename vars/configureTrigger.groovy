31553550b71ec3a622d261615f5c1d6ed763fb59
TheDevSA <ndlovu.code@outlook.com>
2026-05-28 22:47:49 +0200

    fix

--- vars/configureTrigger.groovy
+++ vars/configureTrigger.groovy
@@ -1,15 +1,5 @@
 def call(String branch = 'main') {
-    script {
-        currentBuild.rawBuild.getParent().setScm(
-            new hudson.plugins.git.GitSCM(
-                hudson.plugins.git.GitSCM.createRepoList(env.GIT_URL, 'origin'),
-                [new hudson.plugins.git.BranchSpec("*/${branch}")],
-                false, [], null, null, []
-            )
-        )
-
-        properties([
-            pipelineTriggers([githubPush()])
-        ])
-    }
+    properties([
+        pipelineTriggers([githubPush()])
+    ])
 }
